f2ab24d21d2ffb9ae5cd1e54bc9ae1e0c7caf347
Kevin de Roulet <kderoulet@yahoo.com>
2020-03-13 11:35:21 -0700

    Updates

---
+++ precache-manifest.d6e7544f399000fe40d2a3e0e4382914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0785993c3799f49f7fcab88a7eee9180",
+    "url": "/interview-nasa-recreation-clone/index.html"
+  },
+  {
+    "revision": "b15f04b69973317d3273",
+    "url": "/interview-nasa-recreation-clone/static/js/2.3cfb3b3f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/interview-nasa-recreation-clone/static/js/2.3cfb3b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e695ac22669707bc69",
+    "url": "/interview-nasa-recreation-clone/static/js/main.7759960f.chunk.js"
+  },
+  {
+    "revision": "be3ea8728a76ebb97322",
+    "url": "/interview-nasa-recreation-clone/static/js/runtime-main.80e65f9a.js"
+  }
+]);
